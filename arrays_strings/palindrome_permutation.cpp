fe2f9a728cc48baf30d9014d1dc1ede2055187e2
javidam12345 <javiaragoneses1@outlook.com>
2024-03-08 19:17:31 +0100

    ex4

---
+++ arrays_strings/palindrome_permutation.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+
+/*
+ * Given a string, write a function to check if it is a permutation of a palindrome. A
+ * palindrome is a word or phrase that is the same forwards and backwards. A permutation is
+ * a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+ * You can ignore casing and not-letter characters
+ *
+ * Example: Tact Coa
+ * Output: True (permutations : "taco cat", "atco cta", etc.)
+ *
+ */
+
+/*
+ * 0001000100
+ * 0001000000
+ *
+ */
+
+int convert_to_alphabet_pos(char c){
+    if (c >=  'A' && c <= 'Z')
+    {
+        return c - 'A';
+    }
+    else if (c >= 'a' && c <= 'z')
+    {
+        return c - 'a';
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+
+bool palindrome_permutation(char *str) {
+    int map_of_bits = 0;
+    int on = 0;
+    int i = 0;
+    while(*str)
+    {
+        int temp = convert_to_alphabet_pos(*str);
+        if (temp != -1) {
+            int mask = 1 << temp;
+            map_of_bits ^= mask;
+            on += map_of_bits & mask ? 1 : -1;
+            i++;
+        }
+        str++;
+    }
+    return ((i % 2 == 0) && on == 0) || (i % 2 != 0 && on == 1);
+}
+
+int main()
+{
+
+    char str[] = "aacxd>xaa";
+    std::cout <<  palindrome_permutation(str);
+    return 0;
+}
